b842767f480a06290280d3c6bd098f1359edc94f
Stanislav <ct-ob@mail.ru>
2022-11-11 21:16:26 +0300

    add Semi1_zad1 with kv.koren

---
+++ Semi1_zad1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите целое число и нажмите ENTER: ");
+string? userinput = Console.ReadLine();
+int number = int.Parse(userinput);
+int result = number*number;
+Console.WriteLine($"{number} -> {result}");

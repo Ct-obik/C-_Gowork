2a3ebe5e135a1bff7a706be17973d654965010e2
Stanislav <ct-ob@mail.ru>
2022-11-12 00:07:15 +0300

    add Semi1_commonzad

---
+++ Semi1_commonzad/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Первое число: ");
+int number1 =int.Parse(Console.ReadLine());
+
+Console.WriteLine("Второе число: ");
+int number2 =int.Parse(Console.ReadLine());
+
+int numbersqr=number2*number2;
+
+if (numbersqr == number1)
+{
+    Console.WriteLine("Первое число является");
+}
+else 
+{
+    Console.WriteLine("Первое число НЕ является");
+}

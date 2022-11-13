997c539537e6d41c9be8373708a3f84bf02f3536
Stanislav <ct-ob@mail.ru>
2022-11-13 20:04:44 +0300

    add Semi1_HW2 with find max of 3 numbers

---
+++ Semi1_HW2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое число: ");
+string? numbera = Console.ReadLine();
+int a = int.Parse(numbera);
+Console.WriteLine(a);
+Console.Write("Введите второе число: ");
+string? numberb = Console.ReadLine();
+int b = int.Parse(numberb);
+Console.WriteLine(b);
+Console.Write("Введите третье число: ");
+string? numberc = Console.ReadLine();
+int c = int.Parse(numberc);
+Console.WriteLine(c);
+int max = a;
+if (max<b)
+    {
+    max = b;
+    }
+if (max<c)
+    {
+    max = c;
+    }
+else
+    {
+    Console.WriteLine($"Максимальное число равно {max}");
+    }

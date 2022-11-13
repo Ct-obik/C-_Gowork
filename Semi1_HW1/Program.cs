0f20e4c39ab9ca3921df3a5bb890c050b52a3777
Stanislav <ct-ob@mail.ru>
2022-11-13 19:49:24 +0300

    add Semi1_HW1 with max number

---
+++ Semi1_HW1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+string? numbera = Console.ReadLine();
+int a = int.Parse(numbera);
+Console.WriteLine(a);
+Console.Write("Введите второе число: ");
+string? numberb = Console.ReadLine();
+int b = int.Parse(numberb);
+Console.WriteLine(b);
+if (a>b)
+    {
+    Console.WriteLine($"Максимальное {a}");
+    }
+if (a<b)
+    {
+    Console.WriteLine($"Максимальное {b}");
+    }
+else
+    {
+    Console.WriteLine($"{a} равно {b}");
+    }

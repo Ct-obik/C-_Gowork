f9c083114c5cb2fe08cff71cadad9b361c599f18
Stanislav <ct-ob@mail.ru>
2022-11-13 20:50:35 +0300

    add Semi1_HW3 with /2 or not

---
+++ Semi1_HW3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+double numbera=Convert.ToDouble(Console.ReadLine());
+
+if (numbera%2 ==0)
+{
+    Console.WriteLine($"Число {numbera} чётное");
+}
+else
+{
+Console.WriteLine($"Число {numbera} НЕчётное");
+}

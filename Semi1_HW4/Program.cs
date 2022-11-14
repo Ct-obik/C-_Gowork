e36e5057e53b577d8043aae6a6065c89af4f3af7
Stanislav <ct-ob@mail.ru>
2022-11-14 20:18:06 +0300

    add Semi1_HW4 with /2 from N

---
+++ Semi1_HW4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+double n=Convert.ToDouble(Console.ReadLine());
+int count =0;
+while (count <n)
+
+{
+    int chet=count+1; 
+    if (chet%2 ==0)
+{
+    Console.Write($"{chet} ");
+}
+count++;
+}

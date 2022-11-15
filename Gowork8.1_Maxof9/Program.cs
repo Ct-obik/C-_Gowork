8397e3f90346093cd34c4bfe7dddc9427a79541b
Stanislav <ct-ob@mail.ru>
2022-11-15 14:08:41 +0300

    Add Gowork8.1_Maxof9

---
+++ Gowork8.1_Maxof9/Program.cs
@@ -0,0 +1,11 @@
+﻿int Max(int arg1, int arg2, int arg3)   // Отвечает за поиск MAX из 3
+{
+    int result=arg1;
+    if(arg2>result) result=arg2;
+    if(arg3>result) result=arg3;
+    return result;              // Метод возвращает нам result
+}
+int[] array = { 11, 15, 37, 12, 11, 2, 5, 97, 65 };   // Помним, что индекс начинается с 0
+
+int max =Max(Max(array));
+Console.Write.WriteLine(max);

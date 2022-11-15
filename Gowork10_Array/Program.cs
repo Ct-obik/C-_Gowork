c8131bfa6e9c02052a2d03dfbf905b3546c19de3
Stanislav <ct-ob@mail.ru>
2022-11-15 15:25:00 +0300

    add array for find

--- Gowork10_Array/Program.cs
+++ Gowork10_Array/Program.cs
@@ -18,6 +18,28 @@ void PrintArray(int[] num)  //
         position++;
     }
 }                           // 11-20 Мы выводим на экран
+int IndexOf(int[] numbers, int find)
+{
+    int count = numbers.Length;
+    int index = 0;
+    int position = -1;          // Позваляет нам принимать значение -1, если не будет нужного числа
+                                // так как инчае покажет нам 0.
+    while (index < count)
+    {
+        if (numbers[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}                           // 21-37 Массив для нахождения позиции числа
+
 int[] array = new int[10];  // создаём массив из 10 элементов
 FillArray(array);           // это метод с 1-10 заполняет наш массив
 PrintArray(array);          // этот метод показывает
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);    // Ищем позицию для числа 4 в массиве
+Console.WriteLine(pos);

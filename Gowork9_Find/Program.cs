db9cfd5920cf79445ba158b55832383fb62fa8e8
Stanislav <ct-ob@mail.ru>
2022-11-15 14:19:44 +0300

    add Gowork9_Find with array

---
+++ Gowork9_Find/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 4, 56, 34, 54, 32, 6, 7, 89, 89};
+int n = array.Length;
+int find = 89;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"Нашли число: {index}");
+        break;  // Команда для остановки поиска, чтобы не показывать дублирующие значения
+    }
+    index++;
+}

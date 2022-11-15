fec4633bc2032baa8648c4956e07ff9dd914990d
Stanislav <ct-ob@mail.ru>
2022-11-15 21:02:58 +0300

    add Semi2_Zad1 with parts of number

---
+++ Semi2/Semi2_Zad1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int number = new Random().Next(10, 100);   // Генерируем числа от 10 до 99 (надо добавить 1)
+Console.WriteLine($"{number}-> ");
+
+int a = number / 10;            // Для получения целого числа
+Console.WriteLine($"a = {a} ");
+int b = number % 10;            // Для получения остатка от деления
+Console.WriteLine($"b = {b} ");

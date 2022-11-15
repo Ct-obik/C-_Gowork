41ee06b943f15aa5ad04dc5a994f07590ed8bd96
Stanislav <ct-ob@mail.ru>
2022-11-15 21:34:56 +0300

    add file with parts of numbers

---
+++ Semi2/Semi2_commonzad/Program.cs
@@ -0,0 +1,5 @@
+﻿int number = new Random().Next(100, 1000);   // Генерируем числа от 10 до 99 (надо добавить 1)
+int a = number / 100;            // Для получения целого числа
+int b = number % 10;            // Для получения остатка от деления
+int result = a * 10 + b;
+Console.WriteLine($"{number} -> {result}");

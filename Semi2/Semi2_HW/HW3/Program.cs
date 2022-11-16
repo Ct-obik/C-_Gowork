406ca55d4fe3a692134ce2b7b358a9d7d3c583c6
Stanislav <ct-ob@mail.ru>
2022-11-16 18:43:59 +0300

    add HW3 with find holiday of week

---
+++ Semi2/Semi2_HW/HW3/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int number = int.Parse(Console.ReadLine());
+while (true)
+{
+    Console.WriteLine("Является выходным?");
+    if (number ==1)
+    {
+        Console.WriteLine($"{number} -> Понедельник не выходной");
+        return;
+    }
+    if (number == 2)
+    {
+        Console.WriteLine($"{number} -> Вторник не выходной");
+        return;
+    }
+    if (number ==3)
+    {
+        Console.WriteLine($"{number} -> Среда не выходной");
+        return;
+    }
+    if (number == 4)
+    {
+        Console.WriteLine($"{number} -> Четверг не выходной");
+        return;
+    }
+    if (number ==5)
+    {
+        Console.WriteLine($"{number} -> Пятница не выходной");
+        return;
+    }
+    if (number ==6)
+    {
+        Console.WriteLine($"{number} -> Суббота выходной");
+        return;
+    }
+    if (number == 7)
+    {
+        Console.WriteLine($"{number} -> Воскресенье выходной");
+        return;
+    }
+    else
+    {
+        break;
+    }
+}
+Console.WriteLine("Ошибка ввода, введите ещё раз");

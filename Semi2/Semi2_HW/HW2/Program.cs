ed606237f792cbb58796966220eb945e3bddfe4b
Stanislav <ct-ob@mail.ru>
2022-11-16 18:05:09 +0300

    add HW2 with find 3th of number

---
+++ Semi2/Semi2_HW/HW2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int min = 99;
+while (true)
+{
+    if (number > min)
+    {
+        int a = ((number / 100) % 10);
+        Console.WriteLine($"Третья цифра числа - {a}");
+        return;
+    }
+    else
+    {
+        break;
+    }
+}
+Console.WriteLine("Третьей цифры нет");

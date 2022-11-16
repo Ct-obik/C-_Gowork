6b623fdcd2c8dfcb98b1868e1573ba05bf218863
Stanislav <ct-ob@mail.ru>
2022-11-16 17:29:37 +0300

    add HM1 with find 1 among number

---
+++ Semi2/Semi2_HW/HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int max = 1000;
+int min = 99;
+while (number > min)
+{
+    if (number < max)
+    {
+        int a = (number / 10) % 10;
+        Console.WriteLine($"Вторая цифра числа - {a}");
+        return;
+    }
+    else
+    {
+        break;
+    }
+}
+Console.WriteLine("Ошибка ввода");

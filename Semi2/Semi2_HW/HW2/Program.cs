d26109093a48645c8884cbf16d4bb523dc2c1c54
Stanislav <ct-ob@mail.ru>
2022-11-21 19:15:15 +0300

    v2.0

--- Semi2/Semi2_HW/HW2/Program.cs
+++ Semi2/Semi2_HW/HW2/Program.cs
@@ -1,17 +1,6 @@
 ﻿Console.WriteLine("Введите число:");
 int number = int.Parse(Console.ReadLine());
 int min = 99;
-while (true)
-{
-    if (number > min)
-    {
-        int a = ((number / 100) % 10);
-        Console.WriteLine($"Третья цифра числа - {a}");
-        return;
-    }
-    else
-    {
-        break;
-    }
-}
+while (number > 99) number /= 10;
+Console.WriteLine($"Третья цифра числа - {number % 10}");
 Console.WriteLine("Третьей цифры нет");

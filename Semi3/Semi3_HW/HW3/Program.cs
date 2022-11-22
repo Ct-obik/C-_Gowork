79813257055d3729bada1428196591c29c3063d0
Stanislav <ct-ob@mail.ru>
2022-11-22 17:23:50 +0300

    add HW3 with Tab  of triple 1 - N by method

---
+++ Semi3/Semi3_HW/HW3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+int N = 0;
+InitNumberByUser(ref N);
+PrintTabByCube(N);
+
+static void InitNumberByUser(ref int N)
+{
+    try
+    {
+        Console.Write("Введите число N: ");
+        N = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return;
+    } 
+    Console.Write($"{N} -> ");
+}
+static void PrintTabByCube(int N)
+{
+    for (int i=1; i<=N; i++)
+    {
+        double result = Math.Pow(i, 3);
+        Console.Write($"{result}  ");
+    }
+}

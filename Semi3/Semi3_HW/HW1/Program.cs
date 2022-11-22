27b22b429eaa0d7f443d8e0b9cfc0d4c4f8e5dc8
Stanislav <ct-ob@mail.ru>
2022-11-22 16:19:09 +0300

    add HW1 with palindrom

---
+++ Semi3/Semi3_HW/HW1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+string n;
+while (number > 9999 && number < 100000)
+{
+    int num0 = number / 10000;
+    int num1 = (number / 1000) % 10;
+    int num3 = (number % 100) / 10;
+    int num4 = number % 10;
+    if (num0 == num4 && num1 == num3)
+    {
+        n = "DA";
+    }
+    else
+    {
+        n = "NET";
+    }
+    Console.WriteLine($"{number} -> {n}");
+    return;
+}
+Console.WriteLine("Введено не верное число");

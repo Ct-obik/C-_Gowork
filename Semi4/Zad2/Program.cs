db25421137a2ca9879f23bfe8c38e96ee7b6c07c
Stanislav <ct-ob@mail.ru>
2022-11-23 19:04:42 +0300

    add Zad2 with show col of num

---
+++ Semi4/Zad2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ","Ошибка ввода!");
+int digitsNumber = GetDigitsNumber(num);
+Console.WriteLine($"{num} -> {digitsNumber}");
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
+int GetDigitsNumber(int number)
+{
+    int count = 0;
+    do 
+    {
+        count ++;
+        number /= 10;
+    }
+    while (number != 0);
+    return count;
+}

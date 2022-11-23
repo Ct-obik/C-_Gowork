ee9e1d77af5b40fe9f8e6e4a06262e1a0e8d484c
Stanislav <ct-ob@mail.ru>
2022-11-23 18:49:20 +0300

    add file with sum from 1 to N

---
+++ Semi4/CommonZad1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int number = GetNumberFromUser("Ввести число: ", "Ошибка ввода");
+int sumNumbers = GetSumNumber(number);
+Console.WriteLine($"{number} -> {sumNumbers}");
+
+
+
+
+int GetSumNumber(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+        sum += i;    // =+ - помогает сразу прибовлять к пред. sum = sum + i
+    return sum;
+}
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))       // тоже самое isCorrect == true
+            return userNumber;
+        else    // else можно убрать т.к. выше есть return
+            Console.WriteLine(errorMessage);
+    }
+}

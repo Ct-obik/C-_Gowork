206981fbc1c5d945c865322c95598d6c8565cca9
Stanislav <ct-ob@mail.ru>
2022-11-23 19:20:07 +0300

    add Zad5 with Show random massiv of 0 and 1 by method

---
+++ Semi4/Zad5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке
+Console.Clear();
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 2);
+}
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{col[i]} ");
+}

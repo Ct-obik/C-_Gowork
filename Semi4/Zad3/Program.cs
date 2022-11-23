8fa7fba22cc09a7b98674e052342923ff130570e
Stanislav <ct-ob@mail.ru>
2022-11-23 19:10:01 +0300

    add Zad3 with result 1 to N *

---
+++ Semi4/Zad3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ","Ошибка ввода!");
+int productNumbers = GetProductNumbers(num);
+Console.WriteLine($"{num} -> {productNumbers}");
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
+int GetProductNumbers(int number)
+{
+    int product = 1;
+    for (int i = 1; i <= number; i ++) product *= i;
+    return product;
+}

b3b4973c1d4e65c94bf41659a5e852e7aa0dde0d
Stanislav <ct-ob@mail.ru>
2022-11-29 21:31:46 +0300

    add Zad3 with convert to 2system

---
+++ Semi6/Zad3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Перевести число в двоичную систему
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+int res = ResultAnotherNumber(num);
+Console.WriteLine($"{num} -> {res}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int ResultAnotherNumber(int num)
+{
+    int res = 0;
+    int count = 0;
+    while (num > 0)
+    {
+        res += num % 2 * (int)Math.Pow(10, count);
+        num = num / 2;
+        count += 1;
+    }
+    return res;
+}

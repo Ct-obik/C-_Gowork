00038a3a0158e9a49c8939906701b5b1abd191c9
Stanislav <ct-ob@mail.ru>
2022-11-29 22:03:39 +0300

    add Zad4 with show first numbers Fabonachi

---
+++ Semi6/Zad4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Вывести первые числа Фибоначи с 0 и 1
+// Фибоначи число равно сумме двум пред.
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+int[] array = Fibonachi(num);
+Console.WriteLine(String.Join(" ", array));
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[] Fibonachi(int num)
+{
+    int[] array = new int[num];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < num; i++)
+        array[i] = array[i - 2] + array[i - 1];
+    return array;
+}

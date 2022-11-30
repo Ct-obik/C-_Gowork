ec829092c17e7aa451eb1b01e3c81abdce6f4634
Stanislav <ct-ob@mail.ru>
2022-11-30 23:05:17 +0300

    add Zad4 with factorial by recurtion

---
+++ Lections/Lekcia4/Zad4/Program.cs
@@ -0,0 +1,13 @@
+﻿double Factorial (int n)   // Если число большое, используем double
+{
+    if (n==1) return 1;
+    else return n *Factorial(n-1);
+}
+
+//Console.WriteLine(Factorial(3));  - Показывает нам факториал от 3
+
+// Можно использовать и цикл
+for (int i = 1; i<40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");    // Показывает нам красиво
+}   

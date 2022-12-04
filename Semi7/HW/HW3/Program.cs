50caeec3e7185082a7136028d5e9c993dc6f1493
Stanislav <ct-ob@mail.ru>
2022-12-04 14:58:27 +0300

    add HW3 with find Sum(j)/i in massive

---
+++ Semi7/HW/HW3/Program.cs
@@ -0,0 +1,61 @@
+﻿// 52: Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Clear();
+int row = GetNumberFromUser("Введите количество строк массива: ", "Ошибка ввода!");
+int col = GetNumberFromUser("Введите количество столбцов массива: ", "Ошибка ввода!");
+int[,] matrix = GetArray(row, col, -10, 10);
+PrintArray(matrix);
+Console.WriteLine("Среднее арифметическое в столбцах:");
+ShowResult(matrix);
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1); ;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ShowResult(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        sum = Math.Round((sum / row), 2);
+        Console.Write($"{sum}, ");
+    }
+}

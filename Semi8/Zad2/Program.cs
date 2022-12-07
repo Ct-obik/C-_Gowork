8537d3a8107659fe38c75608dc9224937d77cdf8
Stanislav <ct-ob@mail.ru>
2022-12-07 21:47:49 +0300

    Zad2 with change lines with columns in massive

---
+++ Semi8/Zad2/Program.cs
@@ -0,0 +1,55 @@
+﻿// 55 Создайте двумерный массив. Напишите программу,
+// которая заменяет смтроки на столбцы. В случае,
+// если это невозможно, программа должная вывести сообщение для пользователя.
+
+Console.Clear();
+Console.Write("Введите кол-во строк массива: ", "Ошибка ввода!");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите кол-во столбцов массива: ", "Ошибка ввода!");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 10);  
+int[,] NewReversArr = ReversArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(NewReversArr);
+                  
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)   
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ReversArray(int[,] arr)
+{
+    int[,] revers = new int[arr.GetLength(1), arr.GetLength(0)];
+    for (int i = 0; i < revers.GetLength(0); i++)
+    {
+        for (int j = 0; j < revers.GetLength(1); j++)
+        {
+            revers[i, j] = arr[j, i];
+        }
+    }
+    return revers;
+}

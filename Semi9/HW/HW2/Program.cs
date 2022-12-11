c79a462844d71ac2271576c3a2dc16038906ab7c
Stanislav <ct-ob@mail.ru>
2022-12-11 20:58:30 +0300

    HW2 with Sum of[M to N] by Recurtion

---
+++ Semi9/HW/HW2/Program.cs
@@ -0,0 +1,33 @@
+﻿// 66 Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N (включая сами числа).
+
+Console.Clear();
+int m = GetNumberFromUser("Введите число M: ", "Ошибка ввода!");
+int n = GetNumberFromUser("Введите число N: ", "Ошибка ввода!");
+Console.WriteLine($"Сумма всех чисел от {m} и до {n} = {GetSum(m, n)}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetSum(int m, int n)
+{
+    if (m < n)
+    {
+        if (m == n) return m;
+        else return m + GetSum((m + 1), n);
+    }
+    else
+    {
+        if (m == n) return m;
+        else return m + GetSum((m - 1), n);
+    }
+}

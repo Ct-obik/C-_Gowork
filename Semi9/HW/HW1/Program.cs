0f5de79c5ec3a413d7d6491e8da60f503e0537cf
Stanislav <ct-ob@mail.ru>
2022-12-11 19:27:36 +0300

    HW1 with Numbers from N to 1

---
+++ Semi9/HW/HW1/Program.cs
@@ -0,0 +1,27 @@
+﻿// 64 Задайте значение N. Напишите программу,
+//  которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+Console.Clear();
+int a = GetNumberFromUser("Введите число N: ", "Ошибка ввода!");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+string GetSum(int a, int b)
+{
+    if (a <= b) return GetSum(a + 1, b) + $"{a} ";
+    else return string.Empty;
+}
+
+if (a < 1) Console.WriteLine($"Введите другое число!");
+else Console.WriteLine(GetSum(1, a));

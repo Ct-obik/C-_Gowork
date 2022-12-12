81973bdf26fad25251a936deb0048bc80c9555ed
Stanislav <ct-ob@mail.ru>
2022-12-12 21:14:21 +0300

    User's A in B

---
+++ Semi9/Zad4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+
+int a = GetIntNatFromUser("Введите число A: ");
+int b = GetIntNatFromUser("Введите число B: ");
+
+Console.WriteLine($"A = {a}; B = {b} -> {NumberToPow(a,b)}");
+
+static int NumberToPow(int number, int power)
+{
+    if (power == 1) return number;
+    return number * NumberToPow(number, power - 1);
+}
+
+static int GetIntNatFromUser(string userMsg)
+{
+    while(true)
+    {
+        Console.Write(userMsg);
+        if (int.TryParse(Console.ReadLine(), out int num))
+            if (num >0)
+                return num;
+        Console.WriteLine("Ошибка ввода!");
+    }
+}
